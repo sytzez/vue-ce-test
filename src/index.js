39b525387b708fc8c8d297396d6baa4bb7f61b4f
sytzez <sytzez@gmail.com>
2024-11-12 16:24:55 +0000

    demonstrate what happens when I disable shadowRoot

--- src/index.js
+++ src/index.js
@@ -5,4 +5,11 @@ console.log(MyComponent.styles); // NOTE: Expecting this to contain styles!
 
 const MyComponentElement = defineCustomElement(MyComponent);
 
+// NOTE: Styles work when I disable shadow root, but <slot> requires
+// shadow DOM, and I absolutely need to be able to use <slot>
+
+// const MyComponentElement = defineCustomElement(MyComponent, {
+//   shadowRoot: false,
+// });
+
 customElements.define("my-component", MyComponentElement);
